1c0978a98c531627b324b1f07263712fd918d4a1
ray-vanderloop <rvanderl@uwo.ca>
2024-03-10 15:27:56 -0400

    added footer, need to do some formatting with middle links but i'm happy for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
     return (
@@ -12,12 +14,12 @@ function Footer() {
                         <Link to='/'>Home</Link>
                         <Link to='/contact'>Contact</Link>
                     </div>
-                    <div className='footer-link-items'>
+                    <div className='footer-link-items external'>
                         <h2>Links to Explore</h2>
-                        <Link to={{pathname: "https://noises.online"}} target="_blank">Noises Online - Ambient Noise</Link>
-                        <Link to={{pathname: "https://www.mapcrunch.com"}} target="_blank">Random Street View</Link>
-                        <Link to={{pathname: "https://quickdraw.withgoogle.com"}} target="_blank">Quick, Draw!</Link>
-                        <Link to={{pathname: "https://unshorten.it"}} target="_blank">Unshorten.It</Link>
+                        <p><a href="https://noises.online" target="_blank">Noises Online - Ambient Noise</a></p>
+                        <p><a href="https://www.mapcrunch.com" target="_blank">Random Street View</a></p>
+                        <p><a href="https://quickdraw.withgoogle.com" target="_blank">Quick, Draw!</a></p>
+                        <p><a href="https://unshorten.it" target="_blank">Unshorten.It</a></p>
                     </div>
                     <div className='footer-link-items'>
                         <h2>Contact Info</h2>
@@ -33,27 +35,23 @@ function Footer() {
                             <img to='/' className='logo-img' src='images/logo-white.png'/>
                         </Link>
                     </div>
-                    <small className='website-rights'>Ray Vanderloop, 2023</small>
+                    <small className='website-rights'>Ray Vanderloop, 2024</small>
                     <div className='social-icons'>
-                        
-                        <Link className='social-icon-link instagram'
-                            to={{pathname: "https://www.instagram.com/stella_and_kitty/"}}
-                            target='_blank'
-                            aria-label='Instagram'>
-                                <i className='fab fa-instagram'/>
-                        </Link>
-                        <Link className='social-icon-link linkedin'
-                            to={{pathname: "https://www.linkedin.com/in/rachelvanderloop/"}}
-                            target='_blank'
-                            aria-label='LinkedIn'>
-                                <i className='fab fa-linkedin'/>
-                        </Link>
-                        <Link className='social-icon-link github'
-                            to={{pathname: "https://github.com/ray-vanderloop"}}
-                            target='_blank'
-                            aria-label='LinkedIn'>
-                                <i className='fab fa-github'/>
-                        </Link>
+                        <div className='social-icon-link instagram'>
+                            <a href='https://www.instagram.com/stella_and_kitty/'>
+                                <FontAwesomeIcon icon={faInstagram}/>
+                            </a>
+                        </div>
+                        <div className='social-icon-link linkedin'>
+                            <a href='https://www.linkedin.com/in/rachelvanderloop/'>
+                                <FontAwesomeIcon icon={faLinkedin}/>
+                            </a>
+                        </div>
+                        <div className='social-icon-link github'>
+                            <a href='https://github.com/ray-vanderloop'>
+                                <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
