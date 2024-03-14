037861f836dec21d4458f61bfed2b771d238985a
ray-vanderloop <rvanderl@uwo.ca>
2024-03-13 21:52:05 -0400

    added some better photos to the cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,41 +10,42 @@ function Cards() {
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem 
-                        src='images/rainbow-cloud.jpg'
-                        text='Western International - Ukraine Microsite'
-                        label='Projects'
+                        src='images/netherlands-houses.jpeg'
+                        text='Year Abroad in the Netherlands'
+                        label='Life Events'
                         to={{pathname: "https://unshorten.it"}}
                         />
                         <CardItem 
                         src='images/world-img.jpg'
-                        text='IESC - Peer Guide 30th Anniversary'
-                        label='Personal'
-                        path='/projects'
-                        />
-                        <CardItem 
-                        src='images/meandkitty.jpg'
-                        text='Kitty loves to help me with school'
-                        label='Personal'
-                        path='/projects'
-                        />
-                    </ul>
-                    <ul className='cards__items'>
-                        <CardItem 
-                        src='images/world-img.jpg'
-                        text='Link to cool blog post I like'
-                        label='Personal'
-                        path='/projects'
-                        />
-                        <CardItem 
-                        src='images/meandkitty.jpg'
-                        text='Kitty loves to help me with school'
-                        label='Personal'
+                        text='Curriculum Vitae'
+                        label='Professional'
                         path='/projects'
                         />
                         <CardItem 
                         src='images/graduation.jpg'
                         text='Graduation and Iron Ring Cermony'
                         label='Life Events'
+                        path='/projects'
+                        />
+                        
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem 
+                        src='images/rainbow-cloud.jpg'
+                        text='Photography & Design'
+                        label='Projects'
+                        path='/projects'
+                        />
+                        <CardItem 
+                        src='images/meandkitty.jpg'
+                        text='Timeline'
+                        label='Projects'
+                        path='/projects'
+                        />
+                        <CardItem 
+                        src='images/get-in-touch.jpeg'
+                        text='Get in Touch!'
+                        label='Personal'
                         path='/projects'
                         />
                     </ul>
